27e770a1bf8317236029fce642bc4a122f163ee9
Feiyu Yang <feiyuyang@Feiyus-Air.attlocal.net>
2023-03-18 02:40:52 -0700

    Major change.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 #include <boost/program_options.hpp>
 #include <tbb/task_scheduler_init.h>
 #include "seedTable.cuh"
-#include "kseq.h"
 #include "zlib.h"
 
 // For parsing the command line values
@@ -37,24 +38,32 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    // Read input sequence as kseq_t object
-    timer.Start();
-    fprintf(stdout, "Reading input sequence and compressing to two-bit encoding.\n");
-    gzFile fp = gzopen(refFilename.c_str(), "r");
-    if (!fp) {
-        fprintf(stdout, "ERROR: Cannot open file: %s\n", refFilename.c_str());
-        exit(1);
+    int i,j,l1,l2,t,track;
+    int dist[50][50];
+    //take the strings as input
+    char s1[] = "GCCTAAGCCTAAGCCTAAGCCTAAGCCTAAGCCTAAGCCTAAGCCTAAGC";
+    char s2[] = "CTAAGCCTAAGCCTAAGCCTAAGCCTAAGCCTAAGCCTAAGCCTAAGCCT";
+    //stores the lenght of strings s1 and s2
+    l1 = strlen(s1);
+    l2= strlen(s2);
+    for(i=0;i<=l1;i++) {
+        dist[0][i] = i;
     }
-    kseq_t *record = kseq_init(fp);
-    int n;
-    if ((n = kseq_read(record)) < 0) {
-        fprintf(stdout, "ERROR: No records found!\n");
-        exit(1);
+    for(j=0;j<=l2;j++) {
+        dist[j][0] = j;
     }
-    printf("Sequence name: %s\n", record->name.s);
-    printf("Sequence size: %zu\n", record->seq.l);
-    fprintf(stdout, "Completed in %ld msec \n\n", timer.Stop());
-
+    for (j=1;j<=l1;j++) {
+        for(i=1;i<=l2;i++) {
+            if(s1[i-1] == s2[j-1]) {
+                track= 0;
+            } else {
+                track = 1;
+            }
+            t = MIN((dist[i-1][j]+1),(dist[i][j-1]+1));
+            dist[i][j] = MIN(t,(dist[i-1][j-1]+track));
+        }
+    }
+    cout<<"The Levinstein distance is:"<<dist[l2][l1];
 
     return 0;
 }
